6e50e1ff2400bc90a8051f0d4fd15786846197f4
Junior <jjalves96@gmail.com>
2020-02-27 10:48:11 -0300

    test: commit again

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Routes from './src/routes';
 export default function App() {
   return <>
     {/* Testando commit github */}
+    {/* Testando commit github */}
     <StatusBar barStyle='light-content' backgroundColor='#20C970'/>
     <Routes />
   </>
